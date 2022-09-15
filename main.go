ee2dc396a5e45c595432b6dada5205beb825f8dd
Léon Hollender <hello@leonh.fr>
2022-09-15 13:54:49 +0200

    feat: Export static assets FS from static directory

--- main.go
+++ main.go
@@ -3,26 +3,16 @@ package main
 
 import (
 	"context"
-	"embed"
-	"io/fs"
-	"log"
 	"os"
 	"os/signal"
 
 	"github.com/leonhfr/cete/cmd"
+	"github.com/leonhfr/cete/static"
 )
 
-//go:embed static
-var staticEmbedded embed.FS
-
 func main() {
-	staticFS, err := fs.Sub(fs.FS(staticEmbedded), "static")
-	if err != nil {
-		log.Fatal("could not navigate to the static subtree", err)
-	}
-
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
-	cmd.Execute(ctx, staticFS)
+	cmd.Execute(ctx, static.FileSystem)
 }
---
+++ static/static.go
@@ -0,0 +1,17 @@
+// Package static exports a file system providing access to the static assets.
+package static
+
+import (
+	"embed"
+	"io/fs"
+)
+
+//go:embed *
+var embedded embed.FS
+
+// FileSystem provides access to the static assets.
+var FileSystem fs.FS
+
+func init() {
+	FileSystem = fs.FS(embedded)
+}
